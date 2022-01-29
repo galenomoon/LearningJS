2a4ae9d5745a914d141b321fc3691ded45a323d8
galeno <gui.galenocover@gmail.com>
2022-01-29 16:46:50 -0300

    Start Point | Functions

---
+++ functions/cidadaoPrimeiraClasse.js
@@ -0,0 +1,31 @@
+//Criar de forma literal
+function fun1() { }
+
+//Armazenar em uma variáel
+const fun2 = function () { }
+
+//Armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
+console.log(array[0](2, 3));
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () { return 'Salve' }
+console.log(obj.falar());
+
+// Função como param
+function run(fun) {
+    fun()
+}
+run(function () { console.log('Executando. . .') })
+
+//função retorna/conter função
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c );
+    }
+}
+
+soma(2, 3)(4)
+const cincoMais = soma(2, 3)
+cincoMais(4)
