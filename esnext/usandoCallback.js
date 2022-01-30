1d7618d4ddb234149f6bcd6fe8c5e7a66ecb45df
galeno <gui.galenocover@gmail.com>
2022-01-30 15:12:33 -0300

    Callback #02

--- esnext/usandoCallback.js
+++ esnext/usandoCallback.js
@@ -5,7 +5,7 @@ const http = require('http')
 const getTurma = (letra, callback) => {
     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
     http.get(url, res => {
-
+        let resultado = ''
         res.on('data', dados => {
             resultado += dados
         })
